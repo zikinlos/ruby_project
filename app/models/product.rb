bac2434d423af3ebce269258d7a191f421a86a76
zikinlos <zikin139@gmail.com>
2013-08-11 00:00:25 +0700

    '2nd'

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ActiveRecord::Base
+  attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.00}
+  validates :title, uniqueness: true
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0fa65169450af9630efe827a1d993f93bcf03fe8f3dcd052f51e3fcf8eee5d4a0b764af2caa8d4c88e0398ad2be251740b5ee1876a6692a79eab85e8e9af637c'
